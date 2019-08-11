494599f3d46e7972615bc41bde22bba4141106ab
Tristan <3030096736@qq.com>
2019-08-11 23:30:37 +0800

    first commit

---
+++ showanimation2048.js
@@ -0,0 +1,36 @@
+
+function showNumberWithAnimation( i , j , randNumber ){
+
+    var numberCell = $('#number-cell-' + i + "-" + j );
+
+    numberCell.css('background-color',getNumberBackgroundColor( randNumber ) );
+    numberCell.css('color',getNumberColor( randNumber ) );
+    numberCell.text( getTextValue(randNumber) );
+
+    numberCell.animate({
+        width:1.1*cellSideLength,
+        height:1.1*cellSideLength,
+        top:getPosTop( i , j ),
+        left:getPosLeft( i , j )
+    },100);
+
+    numberCell.animate({
+        width:cellSideLength,
+        height:cellSideLength,
+        top:getPosTop( i , j ),
+        left:getPosLeft( i , j )
+    },200);
+}
+
+function showMoveAnimation( fromx , fromy , tox, toy ){
+
+    var numberCell = $('#number-cell-' + fromx + '-' + fromy );
+    numberCell.animate({
+        top:getPosTop( tox , toy ),
+        left:getPosLeft( tox , toy )
+    },200);
+}
+
+function updateScore( score ){
+    $('#score').text( getTextValue(getmax(score)) );
+}
